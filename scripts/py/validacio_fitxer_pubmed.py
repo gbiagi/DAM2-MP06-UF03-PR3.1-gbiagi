f67178fa12c175a3ca795f8b34dd7563f0e0c7b2
JPG <jordi.pala@gmail.com>
2024-03-14 13:36:18 +0100

    PR3.1: Base de la pràctica

--- scripts/py/validacio_fitxer_pubmed.py
+++ scripts/py/validacio_fitxer_pubmed.py
@@ -1,4 +1,6 @@
 from lxml import etree
+import gzip
+
 
 # Assuming you've downloaded the DTD and saved it as 'pubmed_240101.dtd' in the current directory
 dtd_file = './input/pubmed_240101.dtd'
@@ -7,9 +9,13 @@ dtd_file = './input/pubmed_240101.dtd'
 with open(dtd_file, 'r') as dtd_content:
     dtd = etree.DTD(dtd_content)
 
-# Step 2: Load your XML file
-# Replace 'your_xml_file.xml' with the path to your actual XML file
-xml_document = etree.parse('./input/pubmed24n0001.xml')
+# Step 2: Load your compressed XML file
+# Replace 'your_compressed_xml_file.xml.gz' with the path to your actual compressed XML file
+compressed_xml_file = './input/pubmed24n0001.xml.gz'
+
+# Decompress and parse the XML document
+with gzip.open(compressed_xml_file, 'rb') as xml_content:
+    xml_document = etree.parse(xml_content)
 
 # Step 3: Validate the XML document against the DTD
 is_valid = dtd.validate(xml_document)
